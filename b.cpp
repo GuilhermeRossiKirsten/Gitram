85d55f5c0540cb4ac1fa502f8848595ab6a31c04
Guilherme <guirossik10@gmail.com>
2024-01-10 19:10:27 -0300

    1001

--- b.cpp
+++ b.cpp
@@ -6,8 +6,14 @@ using namespace std;
 
 int main()
 {
+    // int teste3;
+    // cout << teste3 << endl;
+
     
-    
+    // int teste1;
+    // string teste2;
+    // cout << teste1;
+
     // const int price_small{25};
     // const int price_large{35};
     // const double sales_tax{0.06};
@@ -43,22 +49,147 @@ int main()
     // int array[][] = {{1, 2, 3}, {4, 5, 6, 7}};
 
     // cout << array[1][3];
-    vector<int> test_scores{1, 2, 3, 4, 5};
+    // vector<int> test_scores{1, 2, 3, 4, 5};
 
-    test_scores.push_back(90);
+    // test_scores.push_back(90);
 
+
+
+    // int tamanho = sizeof(test_scores) / sizeof(test_scores.at(0));
+
+    // cout << tamanho << endl;
+
+    // for (int i = 0; i < test_scores.size(); i++)
+    // {
+    //     cout << test_scores.at(i) << endl;
+    // }
+
+    // vector<int> vector1 {};
+    // vector<int> vector2 {};
+
+    // vector1.push_back(10);
+    // vector1.push_back(20);
+
+    // cout << vector1.at(0) << endl;
+    // cout << vector1.at(1) << endl;
+    // cout << "tamanho: " << vector1.size() << endl;
+
+    // vector2.push_back(100);
+    // vector2.push_back(200);
+
+    // cout << vector2.at(0) << endl;
+    // cout << vector2.at(1) << endl;
+    // cout << "tamanho: " << vector2.size() << endl;
+
+    // vector<vector<int>> vector_2d {};
+
+    // vector_2d.push_back(vector1);
+    // vector_2d.push_back(vector2);
+
+
+    // cout << "===================" << endl;
+    // cout << vector_2d.at(0).at(0) << endl;
+    // cout << vector_2d.at(0).at(1) << endl;
+    // cout << vector_2d.at(1).at(0) << endl;
+    // cout << vector_2d.at(1).at(1) << endl;
+    // cout << "===================" << endl;
+
+    // vector1.at(0) = 1000;
+
+    // cout << vector_2d.at(0).at(0) << endl;
+    // cout << vector_2d.at(0).at(1) << endl;
+    // cout << vector_2d.at(1).at(0) << endl;
+    // cout << vector_2d.at(1).at(1) << endl;
+    // cout << "===================" << endl;
+
+    // cout << vector1.at(0) << endl;
+    // cout << vector1.at(1) << endl;
+    // cout << "===================" << endl;
+
+    // double temperatures[]{98.6, 95.2, 88.7, 100.7, 89.0};
+    // cout << temperatures[5] << endl;
+
+    // int count{0};
+    // int count2{0};
+
+    // for (int i = 0; i < 10000; i++)
+    // {
+    //     long double caos{(i++ * 2 * 3.14) * (++i * 2 * 3.14)};
+    //     cout << caos << endl;
+    // }
+    // int money;
+    // cin >> money;
+    // int rest{0};
+    // int dollar{0};
+    // int quarters{0};
+    // int dimes{0};
+    // int nickels{0};
+    // int pennies{0};
+
+
+    // dollar = money/100;
+    // rest = money % 100;
     
+    // quarters = rest/25;
+    // rest = money % 25;
+    
+    // dimes = rest/10;
+    // rest = money % 10;
+    
+    // nickels = rest/5;
+    // rest = money % 5;
 
-    int tamanho = sizeof(test_scores) / sizeof(test_scores.at(0));
+    // pennies = rest;
 
-    cout << tamanho << endl;
+    // cout << dollar << endl;
+    // cout << quarters << endl;
+    // cout << dimes << endl;
+    // cout << nickels << endl;
+    // cout << pennies << endl;
 
-    for (int i = 0; i < tamanho; i++)
+    // enum Direction
+    // {
+    //     left,
+    //     rigth,
+    //     up,
+    //     down
+    // };
+
+    // Direction direct{left};
+
+    // switch (direct)
+    // {
+    // case left:
+    //     cout << "left" << endl;
+    //     break;
+    
+    // default:
+
+    //     break;
+    // }
+
+    // int sum{0};
+    // for (int i = 1; i <= 15; i++)
+    // {
+    //     if (i % 2 != 0)
+    //     {
+    //         sum += i;
+    //         cout << i << endl;
+    //     }
+    // }
+
+    // cout << sum << endl;
+
+
+
+    for (auto i : {"T","e","s","t","e"})
     {
-        cout << test_scores.at(i) << endl;
+        cout << i << "\n" << endl;
+
     }
-    test_scores
     
+
+
 
     return 0;
 }
